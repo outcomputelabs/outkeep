b4236a35edf706e4173445559d0bd7eef5eb6120
Jorge Candeias <jorge.candeias@outcompute.com>
2019-10-17 22:40:43 +0100

    Added packages

---
+++ src/Outkeep.Implementations/PingGrain.cs
@@ -0,0 +1,11 @@
+﻿using Orleans;
+using Outkeep.Interfaces;
+using System.Threading.Tasks;
+
+namespace Outkeep.Implementations
+{
+    public class PingGrain : Grain, IPingGrain
+    {
+        public Task PingAsync() => Task.CompletedTask;
+    }
+}
