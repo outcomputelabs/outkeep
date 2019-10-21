490eed9796a9d806f8d1ab71294bb626a09dc9e6
Jorge Candeias <jorge.candeias@outcompute.com>
2019-10-21 12:22:29 +0100

    Added distributed cache extensions

---
+++ src/Outkeep.Client/OutkeepDistributedCacheServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Outkeep.Client
+{
+    public static class OutkeepDistributedCacheServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOutkeepDistributedCache(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            return services.AddSingleton<IDistributedCache, OutkeepDistributedCache>();
+        }
+    }
+}
