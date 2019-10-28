04477b1b4523805c43319afd81f61e2f70acf3b3
Jorge Candeias <jorge.candeias@outcompute.com>
2019-10-28 20:06:48 +0000

    Added application project

---
+++ src/Outkeep.Application/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Outkeep.Application
+{
+    internal sealed class Program
+    {
+        private static Task Main(string[] args)
+        {
+            return new HostBuilder()
+                .ConfigureHostConfiguration(config =>
+                {
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                })
+                .ConfigureLogging((context, logging) =>
+                {
+                })
+                .ConfigureServices((context, services) =>
+                {
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
