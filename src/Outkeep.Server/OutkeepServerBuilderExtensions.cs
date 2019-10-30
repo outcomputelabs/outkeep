4941baac74b51925cdd56c4198a1a878f484e4ba
Jorge Candeias <jorge.candeias@outcompute.com>
2019-10-30 19:14:35 +0000

    Cleanup

--- src/Outkeep.Server/OutkeepServerBuilderExtensions.cs
+++ src/Outkeep.Server/OutkeepServerBuilderExtensions.cs
@@ -5,21 +5,6 @@ namespace Outkeep.Server
 {
     public static class OutkeepServerBuilderExtensions
     {
-        public static IOutkeepServerBuilder UseDefaults(this IOutkeepServerBuilder outkeep)
-        {
-            if (outkeep == null) throw new ArgumentNullException(nameof(outkeep));
-
-            outkeep.ConfigureServices(services =>
-            {
-            });
-
-            outkeep.ConfigureSilo(orleans =>
-            {
-            });
-
-            return outkeep;
-        }
-
         public static IOutkeepServerBuilder UseLocalhostSetup(this IOutkeepServerBuilder outkeep)
         {
             if (outkeep == null) throw new ArgumentNullException(nameof(outkeep));
