93317ad433120e4980ed1e7224b863f18c73a879
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-05 19:16:44 +0000

    Added comments

--- src/Outkeep.Hosting/IOutkeepServerBuilder.cs
+++ src/Outkeep.Hosting/IOutkeepServerBuilder.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Orleans.Hosting;
 using System;
-using HostBuilderContext = Microsoft.Extensions.Hosting.HostBuilderContext;
 
 namespace Outkeep.Hosting
 {
+    /// <summary>
+    /// Offers a convenience integration point for optional extensions.
+    /// </summary>
     public interface IOutkeepServerBuilder
     {
+        /// <summary>
+        /// Configures services upon the outkeep server instance.
+        /// </summary>
         IOutkeepServerBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configure);
 
+        /// <summary>
+        /// Configures the underlying Orleans silo that supports the outkeep server instance.
+        /// </summary>
         IOutkeepServerBuilder ConfigureSilo(Action<HostBuilderContext, ISiloBuilder> configure);
     }
 }
