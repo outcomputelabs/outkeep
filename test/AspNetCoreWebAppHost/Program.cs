f0b899fc3d3139b39348f5f1eb69cc7f78e0e790
Jorge Candeias <candeias@fastmail.fm>
2019-11-11 23:03:55 +0000

    Removed command line configuration source

--- test/AspNetCoreWebAppHost/Program.cs
+++ test/AspNetCoreWebAppHost/Program.cs
@@ -5,13 +5,13 @@ namespace AspNetCoreWebAppHost
 {
     public sealed class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder().Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
