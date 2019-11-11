41a8c3369aea7223caeed9cb5c61cc88a778f940
Jorge Candeias <candeias@fastmail.fm>
2019-11-11 23:05:41 +0000

    Made class static

--- test/AspNetCoreWebAppHost/Program.cs
+++ test/AspNetCoreWebAppHost/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreWebAppHost
 {
-    public sealed class Program
+    public static class Program
     {
         public static void Main()
         {
