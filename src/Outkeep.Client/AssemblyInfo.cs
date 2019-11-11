43fc69aca9f03d785fd486dca2fcc061b62cac8f
Jorge Candeias <candeias@fastmail.fm>
2019-11-11 23:28:13 +0000

    Added test

---
+++ src/Outkeep.Client/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Outkeep.Client.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ test/Outkeep.Client.Tests/OutkeepClientHostedServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Client.Tests
+{
+    public class OutkeepClientHostedServiceTests
+    {
+        [Fact]
+        public async Task Cycles()
+        {
+            var logger = Mock.Of<ILogger<OutkeepClientHostedService>>();
+            var service = new OutkeepClientHostedService(logger);
+
+            await service.StartAsync(default).ConfigureAwait(false);
+            await service.StopAsync(default).ConfigureAwait(false);
+        }
+    }
+}
