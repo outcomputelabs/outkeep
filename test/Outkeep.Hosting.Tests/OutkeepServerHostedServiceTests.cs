75da4ee054098668abd98648052b64fd963d3500
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-12 12:32:17 +0000

    Added test

---
+++ test/Outkeep.Hosting.Tests/OutkeepServerHostedServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Hosting.Tests
+{
+    public class OutkeepServerHostedServiceTests
+    {
+        [Fact]
+        public async Task Cycles()
+        {
+            var logger = Mock.Of<ILogger<OutkeepServerHostedService>>();
+            var service = new OutkeepServerHostedService(logger);
+            await service.StartAsync(default).ConfigureAwait(false);
+            await service.StopAsync(default).ConfigureAwait(false);
+        }
+    }
+}
