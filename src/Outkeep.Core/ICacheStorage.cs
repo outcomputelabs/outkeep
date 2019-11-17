25134362cf41ea062100618e7c2372b5714f7bb2
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-17 22:07:49 +0000

    Added ICacheStorage draft

---
+++ src/Outkeep.Core/ICacheStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Outkeep.Core
+{
+    public interface ICacheStorage
+    {
+        Task<(byte[] Value, DateTimeOffset? AbsoluteExpiration, TimeSpan? SlidingExpiration)> TryReadAsync(string key);
+
+        Task WriteAsync(string key, byte[] value, DateTimeOffset? absoluteExpiration, TimeSpan? slidingExpiration);
+
+        Task ClearAsync(string key);
+    }
+}
