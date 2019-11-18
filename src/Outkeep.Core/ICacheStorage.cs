4821040f690ffc052309e72e04ee976c74795b6d
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-18 04:19:21 +0000

    Added MemoryCacheStorage draft

--- src/Outkeep.Core/ICacheStorage.cs
+++ src/Outkeep.Core/ICacheStorage.cs
@@ -5,7 +5,7 @@ namespace Outkeep.Core
 {
     public interface ICacheStorage
     {
-        Task<(byte[] Value, DateTimeOffset? AbsoluteExpiration, TimeSpan? SlidingExpiration)> TryReadAsync(string key);
+        Task<(byte[] Value, DateTimeOffset? AbsoluteExpiration, TimeSpan? SlidingExpiration)?> TryReadAsync(string key);
 
         Task WriteAsync(string key, byte[] value, DateTimeOffset? absoluteExpiration, TimeSpan? slidingExpiration);
 
---
+++ src/Outkeep.Core/MemoryCacheStorage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Outkeep.Core
+{
+    public class MemoryCacheStorage : ICacheStorage
+    {
+        private readonly ConcurrentDictionary<string, (byte[] Value, DateTimeOffset? AbsoluteExpiration, TimeSpan? SlidingExpiration)> storage =
+            new ConcurrentDictionary<string, (byte[] Value, DateTimeOffset? AbsoluteExpiration, TimeSpan? SlidingExpiration)>();
+
+        public Task ClearAsync(string key)
+        {
+            storage.TryRemove(key, out _);
+            return Task.CompletedTask;
+        }
+
+        public Task<(byte[] Value, DateTimeOffset? AbsoluteExpiration, TimeSpan? SlidingExpiration)?> TryReadAsync(string key)
+        {
+            if (storage.TryGetValue(key, out var value))
+            {
+                return Task.FromResult<(byte[], DateTimeOffset?, TimeSpan?)?>(value);
+            }
+            return null;
+        }
+
+        public Task WriteAsync(string key, byte[] value, DateTimeOffset? absoluteExpiration, TimeSpan? slidingExpiration)
+        {
+            storage[key] = (value, absoluteExpiration, slidingExpiration);
+            return Task.CompletedTask;
+        }
+    }
+}
