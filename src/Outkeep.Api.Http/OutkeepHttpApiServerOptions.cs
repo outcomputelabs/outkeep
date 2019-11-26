7c79791527f49f2a20b6ddc9acb6834587b88223
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 17:57:49 +0000

    Added comments

--- src/Outkeep.Api.Http/OutkeepHttpApiServerOptions.cs
+++ src/Outkeep.Api.Http/OutkeepHttpApiServerOptions.cs
@@ -3,11 +3,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Outkeep.Api.Http
 {
+    /// <summary>
+    /// Options for configuring the <see cref="OutkeepHttpApiHostedService"/> instance.
+    /// </summary>
     public class OutkeepHttpApiServerOptions
     {
+        /// <summary>
+        /// The URI for the Outkeep HTTP API to listen on.
+        /// </summary>
         [Required]
         public Uri ApiUri { get; set; }
 
+        /// <summary>
+        /// The user interface title for the Outkeep HTTP API instance.
+        /// </summary>
         [Required]
         public string Title { get; set; } = nameof(Outkeep);
     }
