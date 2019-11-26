f1039dffe80d0443f585318e5064cf6e674d54c1
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 21:10:29 +0000

    Added test

---
+++ test/Outkeep.Api.Http.Tests/CacheControllerV1Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Orleans;
+using Orleans.Concurrency;
+using Outkeep.Api.Http.Controllers.V1;
+using Outkeep.Interfaces;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Api.Http.Tests
+{
+    public class CacheControllerV1Tests
+    {
+        private static readonly byte[] empty = null;
+
+        [Fact]
+        public async Task GetNonExistingKeyReturnsNoContent()
+        {
+            var key = Guid.NewGuid().ToString();
+            var factory = Mock.Of<IGrainFactory>(x => x.GetGrain<ICacheGrain>(key, null).GetAsync() == Task.FromResult(empty.AsImmutable()));
+            var controller = new CacheController(factory);
+
+            var result = await controller.GetAsync(key).ConfigureAwait(false);
+
+            Assert.IsType<NoContentResult>(result.Result);
+        }
+    }
+}
