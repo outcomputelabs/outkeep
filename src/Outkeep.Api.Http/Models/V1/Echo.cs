d3d90662f94690385bc53f091d941ec3eb17ef7e
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 17:46:59 +0000

    Added comments

--- src/Outkeep.Api.Http/Models/V1/Echo.cs
+++ src/Outkeep.Api.Http/Models/V1/Echo.cs
@@ -2,10 +2,24 @@
 
 namespace Outkeep.Api.Http.Models.V1
 {
+    /// <summary>
+    /// The response from an echo request.
+    /// </summary>
     public class Echo
     {
+        /// <summary>
+        /// Correlation identifier for the request.
+        /// </summary>
         public Guid ActivityId { get; set; }
+
+        /// <summary>
+        /// The message present in the request.
+        /// </summary>
         public string Message { get; set; }
+
+        /// <summary>
+        /// The API version that fulfilled the request.
+        /// </summary>
         public string Version { get; set; }
     }
 }
