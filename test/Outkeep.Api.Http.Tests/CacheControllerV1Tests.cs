6e9989a29e5451bc458fd80c7d20b4afdb1fea59
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 21:14:55 +0000

    Added test

--- test/Outkeep.Api.Http.Tests/CacheControllerV1Tests.cs
+++ test/Outkeep.Api.Http.Tests/CacheControllerV1Tests.cs
@@ -12,18 +12,33 @@ namespace Outkeep.Api.Http.Tests
 {
     public class CacheControllerV1Tests
     {
-        private static readonly byte[] empty = null;
+        private static readonly byte[] absent = null;
+        private static readonly byte[] present = Array.Empty<byte>();
 
         [Fact]
         public async Task GetNonExistingKeyReturnsNoContent()
         {
             var key = Guid.NewGuid().ToString();
-            var factory = Mock.Of<IGrainFactory>(x => x.GetGrain<ICacheGrain>(key, null).GetAsync() == Task.FromResult(empty.AsImmutable()));
+            var factory = Mock.Of<IGrainFactory>(x => x.GetGrain<ICacheGrain>(key, null).GetAsync() == Task.FromResult(absent.AsImmutable()));
             var controller = new CacheController(factory);
 
             var result = await controller.GetAsync(key).ConfigureAwait(false);
 
             Assert.IsType<NoContentResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task GetExistingKeyReturnsOk()
+        {
+            var key = Guid.NewGuid().ToString();
+            var factory = Mock.Of<IGrainFactory>(x => x.GetGrain<ICacheGrain>(key, null).GetAsync() == Task.FromResult(present.AsImmutable()));
+            var controller = new CacheController(factory);
+
+            var result = await controller.GetAsync(key).ConfigureAwait(false);
+
+            var ok = Assert.IsType<OkObjectResult>(result.Result);
+            var content = Assert.IsType<byte[]>(ok.Value);
+            Assert.Same(present, content);
         }
     }
 }
