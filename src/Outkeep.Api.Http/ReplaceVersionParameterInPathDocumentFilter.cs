4ac938a6303a4cc77f6031c951e7cea4f702f0a5
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 19:12:14 +0000

    Added comments

--- src/Outkeep.Api.Http/ReplaceVersionParameterInPathDocumentFilter.cs
+++ src/Outkeep.Api.Http/ReplaceVersionParameterInPathDocumentFilter.cs
@@ -4,8 +4,14 @@ using System;
 
 namespace Outkeep.Api.Http
 {
+    /// <summary>
+    /// Replaces the version parameter in all paths with the specific version number for that path.
+    /// </summary>
     public class ReplaceVersionParameterInPathDocumentFilter : IDocumentFilter
     {
+        /// <summary>
+        /// Replaces the version parameter in all paths with the specific version the user is browsing.
+        /// </summary>
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
             if (swaggerDoc == null) throw new ArgumentNullException(nameof(swaggerDoc));
