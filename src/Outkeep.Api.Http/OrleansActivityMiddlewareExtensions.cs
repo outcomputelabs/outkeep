0d38c55dea1f51d515c5841f1582d9b30fb34171
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 17:53:45 +0000

    Added comments and helper method

--- src/Outkeep.Api.Http/OrleansActivityMiddlewareExtensions.cs
+++ src/Outkeep.Api.Http/OrleansActivityMiddlewareExtensions.cs
@@ -1,18 +1,37 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Outkeep.Api.Http
 {
+    /// <summary>
+    /// Extension methods for adding an <see cref="OrleansActivityMiddleware"/> to an <see cref="IApplicationBuilder"/> pipeline.
+    /// </summary>
     public static class OrleansActivityMiddlewareExtensions
     {
+        /// <summary>
+        /// Adds an <see cref="OrleansActivityMiddleware"/> to the <see cref="IApplicationBuilder"/> pipeline.
+        /// </summary>
         public static IApplicationBuilder UseActivityMiddleware(this IApplicationBuilder builder)
         {
             return builder.UseMiddleware<OrleansActivityMiddleware>();
         }
 
+        /// <summary>
+        /// Adds the <see cref="OrleansActivityMiddleware"/> as a factory middleware to the <see cref="IServiceCollection"/>.
+        /// </summary>
         public static IServiceCollection AddActivityMiddleware(this IServiceCollection services)
         {
             return services.AddTransient<OrleansActivityMiddleware>();
+        }
+
+        /// <summary>
+        /// Adds the <see cref="OrleansActivityMiddleware"/> as a factory middleware to the <see cref="IServiceCollection"/> if it has not been added yet.
+        /// </summary>
+        public static IServiceCollection TryAddActivityMiddleware(this IServiceCollection services)
+        {
+            services.TryAddTransient<OrleansActivityMiddleware>();
+            return services;
         }
     }
 }
