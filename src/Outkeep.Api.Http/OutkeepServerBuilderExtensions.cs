ee9b90e6330617bdaa09c2df3ee6294f69f4d364
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-26 19:08:11 +0000

    Added comments and cleanup

--- src/Outkeep.Api.Http/OutkeepServerBuilderExtensions.cs
+++ src/Outkeep.Api.Http/OutkeepServerBuilderExtensions.cs
@@ -4,15 +4,22 @@ using System;
 
 namespace Outkeep.Hosting
 {
+    /// <summary>
+    /// Extension methods for adding an <see cref="OutkeepHttpApiHostedService"/> to an <see cref="IOutkeepServerBuilder"/>.
+    /// </summary>
     public static class OutkeepServerBuilderExtensions
     {
+        /// <summary>
+        /// Sets up an Outkeep HTTP API on this <see cref="IOutkeepServerBuilder"/> instance.
+        /// Successive calls to this extension method are cumulative and will only create a single API instance.
+        /// </summary>
         public static IOutkeepServerBuilder UseHttpApi(this IOutkeepServerBuilder outkeep, Action<OutkeepHttpApiServerOptions> configure)
         {
             if (outkeep == null) throw new ArgumentNullException(nameof(outkeep));
 
             return outkeep.ConfigureServices((context, services) =>
             {
-                services.AddHostedService<OutkeepHttpApiHostedService>();
+                services.TryAddHostedService<OutkeepHttpApiHostedService>();
                 services.AddOptions<OutkeepHttpApiServerOptions>()
                     .Configure(configure)
                     .ValidateDataAnnotations();
---
+++ src/Outkeep.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Utility extensions for <see cref="IServiceCollection"/> instances.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds <typeparamref name="T"/> as an <see cref="IHostedService"/> to the <see cref="IServiceCollection"/> if it is not yet present.
+        /// </summary>
+        public static void TryAddHostedService<T>(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            var service = new ServiceDescriptor(typeof(IHostedService), typeof(T), ServiceLifetime.Singleton);
+            if (services.Contains(service)) return;
+            services.Add(service);
+        }
+    }
+}
