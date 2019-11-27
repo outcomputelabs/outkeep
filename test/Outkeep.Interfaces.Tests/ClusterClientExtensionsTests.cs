86432697a6725d8a3dbd760b9c5a55402c377876
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 18:34:40 +0000

    Added test

---
+++ test/Outkeep.Interfaces.Tests/ClusterClientExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using Orleans;
+using System;
+using Xunit;
+
+namespace Outkeep.Interfaces.Tests
+{
+    public class ClusterClientExtensionsTests
+    {
+        [Fact]
+        public void GetCacheGrainReturnsCacheGrain()
+        {
+            var grain = Mock.Of<ICacheGrain>();
+            var key = Guid.NewGuid().ToString();
+            var client = Mock.Of<IClusterClient>(x => x.GetGrain<ICacheGrain>(key, null) == grain);
+
+            var result = client.GetCacheGrain(key);
+
+            Assert.Same(grain, result);
+        }
+    }
+}
