28ec046d7bfb190403d35775e3f6106c827cfe97
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 18:46:16 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/DefaultSystemClockTests.cs
@@ -0,0 +1,20 @@
+﻿using Outkeep.Core;
+using System;
+using Xunit;
+
+namespace Outkeep.Interfaces.Tests
+{
+    public class DefaultSystemClockTests
+    {
+        [Fact]
+        public void ReturnsUtcNow()
+        {
+            var clock = new DefaultSystemClock();
+
+            var result = clock.UtcNow;
+            var now = DateTimeOffset.UtcNow;
+
+            Assert.InRange(result, now.AddMilliseconds(-100), now.AddMilliseconds(100));
+        }
+    }
+}
