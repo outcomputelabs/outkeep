53e6f0e96a5bbca5efc8892ce66b5b512c9606d7
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 19:34:27 +0000

    Added test

---
+++ test/Outkeep.Hosting.Tests/HostBuilderExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Outkeep.Hosting.Tests
+{
+    public class HostBuilderExtensionsTests
+    {
+        [Fact]
+        public void UseOutkeepServerCallsActionsOnBuild()
+        {
+            var builder = new HostBuilder();
+            var called = false;
+
+            var result = builder.UseOutkeepServer((context, outkeep) =>
+            {
+                called = true;
+            });
+
+            Assert.False(called);
+
+            builder.Build();
+
+            Assert.Same(builder, result);
+            Assert.True(called);
+        }
+    }
+}
