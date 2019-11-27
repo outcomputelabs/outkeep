65646602a529040d116bdf5c75dc397077c0bcbb
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 19:55:00 +0000

    Added test

---
+++ test/Outkeep.Client.Tests/OutkeepDistributedCacheTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using Orleans;
+using Orleans.Concurrency;
+using Outkeep.Interfaces;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Client.Tests
+{
+    public class OutkeepDistributedCacheTests
+    {
+        [Fact]
+        public async Task GetAsyncCallsGrain()
+        {
+            var key = Guid.NewGuid().ToString();
+            var value = Guid.NewGuid().ToByteArray();
+            var factory = Mock.Of<IGrainFactory>(x => x.GetGrain<ICacheGrain>(key, null).GetAsync() == Task.FromResult(value.AsImmutable()));
+            var cache = new OutkeepDistributedCache(factory);
+
+            var result = await cache.GetAsync(key).ConfigureAwait(false);
+
+            Assert.Same(value, result);
+            Mock.Get(factory).VerifyAll();
+        }
+    }
+}
