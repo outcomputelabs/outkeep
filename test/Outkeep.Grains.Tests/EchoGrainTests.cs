6a2830f3286fcb1f9295814f79d58bd207f6190d
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 20:22:34 +0000

    Added test

---
+++ test/Outkeep.Grains.Tests/EchoGrainTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Grains.Tests
+{
+    public class EchoGrainTests
+    {
+        [Fact]
+        public async Task Echoes()
+        {
+            var grain = new EchoGrain();
+            var message = Guid.NewGuid().ToString();
+
+            await grain.OnActivateAsync().ConfigureAwait(false);
+            var result = await grain.EchoAsync(message).ConfigureAwait(false);
+
+            Assert.Equal(message, result);
+        }
+    }
+}
