bf650074eac1eed0020cb9af6d6b30861e4ab63f
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 19:44:24 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/MemoryCacheStorageTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class MemoryCacheStorageTests
+    {
+        [Fact]
+        public async Task Cycles()
+        {
+            var key = Guid.NewGuid().ToString();
+            var value = Guid.NewGuid().ToByteArray();
+            var absoluteExpiration = DateTimeOffset.UtcNow.AddHours(1);
+            var slidingExpiration = TimeSpan.FromMinutes(1);
+            var storage = new MemoryCacheStorage();
+
+            // attempt to read empty state
+            var result = await storage.TryReadAsync(key).ConfigureAwait(false);
+            Assert.False(result.HasValue);
+
+            // write some state
+            await storage.WriteAsync(key, value, absoluteExpiration, slidingExpiration).ConfigureAwait(false);
+
+            // attempt to read that state
+            result = await storage.TryReadAsync(key).ConfigureAwait(false);
+            Assert.True(result.HasValue);
+            Assert.Same(value, result.Value.Value);
+            Assert.Equal(absoluteExpiration, result.Value.AbsoluteExpiration);
+            Assert.Equal(slidingExpiration, result.Value.SlidingExpiration);
+
+            // clear the state
+            await storage.ClearAsync(key).ConfigureAwait(false);
+
+            // attempt to read the cleared state
+            result = await storage.TryReadAsync(key).ConfigureAwait(false);
+            Assert.False(result.HasValue);
+        }
+    }
+}
