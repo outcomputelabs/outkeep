3acc1d7c3cefc1460ef355ef8d0d40a8d5f96813
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-27 11:04:57 +0000

    Added test

---
+++ test/Outkeep.Api.Http.Tests/OrleansActivityMiddlewareExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using Xunit;
+
+namespace Outkeep.Api.Http.Tests
+{
+    public class OrleansActivityMiddlewareExtensionsTests
+    {
+        [Fact]
+        public void UseActivityMiddleware()
+        {
+            var builder = Mock.Of<IApplicationBuilder>();
+            Mock.Get(builder).Setup(x => x.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>())).Returns(builder);
+
+            var result = builder.UseActivityMiddleware();
+
+            Mock.Get(builder).VerifyAll();
+            Assert.Same(builder, result);
+        }
+    }
+}
