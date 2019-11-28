cd326cbd227ec474c120d3e5bcff1352521dafb3
Jorge Candeias <jorge.candeias@outcompute.com>
2019-11-28 12:11:42 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/OutkeepServerBuilderMemoryCacheStorageExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using System;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class OutkeepServerBuilderMemoryCacheStorageExtensionsTests
+    {
+        [Fact]
+        public void AddMemoryCacheStorageConfiguresServices()
+        {
+            var services = new ServiceCollection();
+
+            var builder = Mock.Of<IOutkeepServerBuilder>();
+            Mock.Get(builder)
+                .Setup(x => x.ConfigureServices(It.IsAny<Action<HostBuilderContext, IServiceCollection>>()))
+                .Callback((Action<HostBuilderContext, IServiceCollection> action) => action(null, services))
+                .Returns(builder);
+
+            var result = builder.AddMemoryCacheStorage();
+
+            Mock.Get(builder).VerifyAll();
+            Assert.Same(builder, result);
+
+            var provider = services.BuildServiceProvider();
+            Assert.IsType<MemoryCacheStorage>(services.BuildServiceProvider().GetService<ICacheStorage>());
+        }
+    }
+}
