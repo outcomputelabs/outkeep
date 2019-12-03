2681b9f98c41b57f3e094af105a6df12b34d37e0
Jorge Candeias <candeias@fastmail.fm>
2019-12-03 22:56:45 +0000

    Added test

--- test/Outkeep.Client.Tests/HostBuilderExtensionsTests.cs
+++ test/Outkeep.Client.Tests/HostBuilderExtensionsTests.cs
@@ -6,12 +6,31 @@ namespace Outkeep.Client.Tests
     public class HostBuilderExtensionsTests
     {
         [Fact]
-        public void UseOutkeepClientCallsActionsOnBuild()
+        public void UseOutkeepClientCallsActionsOnBuild1()
         {
             var builder = new HostBuilder();
             var called = false;
 
             var result = builder.UseOutkeepClient((context, outkeep) =>
+            {
+                called = true;
+            });
+
+            Assert.False(called);
+
+            builder.Build();
+
+            Assert.Same(builder, result);
+            Assert.True(called);
+        }
+
+        [Fact]
+        public void UseOutkeepClientCallsActionsOnBuild2()
+        {
+            var builder = new HostBuilder();
+            var called = false;
+
+            var result = builder.UseOutkeepClient(outkeep =>
             {
                 called = true;
             });
