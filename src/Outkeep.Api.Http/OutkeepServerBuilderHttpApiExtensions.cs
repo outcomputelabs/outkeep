7ec5b1ffa4c60466014649ec8b400ba98fcf33a6
Jorge Candeias <candeias@fastmail.fm>
2019-12-04 00:22:26 +0000

    Renamed extension classes

--- src/Outkeep.Api.Http/OutkeepServerBuilderHttpApiExtensions.cs
+++ src/Outkeep.Api.Http/OutkeepServerBuilderHttpApiExtensions.cs
@@ -7,7 +7,7 @@ namespace Outkeep.Hosting
     /// <summary>
     /// Extension methods for adding an <see cref="OutkeepHttpApiHostedService"/> to an <see cref="IOutkeepServerBuilder"/>.
     /// </summary>
-    public static class OutkeepServerBuilderExtensions
+    public static class OutkeepServerBuilderHttpApiExtensions
     {
         /// <summary>
         /// Sets up an Outkeep HTTP API on this <see cref="IOutkeepServerBuilder"/> instance.
