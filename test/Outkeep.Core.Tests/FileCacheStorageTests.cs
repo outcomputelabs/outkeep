893cbeb645c396d7b3e50a371015a4c799447f96
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-04 09:08:30 +0000

    Cleanup

--- test/Outkeep.Core.Tests/FileCacheStorageTests.cs
+++ test/Outkeep.Core.Tests/FileCacheStorageTests.cs
@@ -6,7 +6,6 @@ using System.Collections.Immutable;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
