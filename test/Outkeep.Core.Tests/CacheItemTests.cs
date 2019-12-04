7fe9964f5b105042cafdf3bba685fdec10058454
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-04 18:20:36 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/CacheItemTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class CacheItemTests
+    {
+        [Fact]
+        public void Constructs()
+        {
+            // arrange
+            var value = Guid.NewGuid().ToByteArray();
+            var absoluteExpiration = new DateTimeOffset(1, 2, 3, 4, 5, 6, TimeSpan.Zero);
+            var slidingExpiration = new TimeSpan(1, 2, 3);
+
+            // act
+            var item = new CacheItem(value, absoluteExpiration, slidingExpiration);
+
+            // assert
+            Assert.Same(value, item.Value);
+            Assert.Equal(absoluteExpiration, item.AbsoluteExpiration);
+            Assert.Equal(slidingExpiration, item.SlidingExpiration);
+        }
+    }
+}
