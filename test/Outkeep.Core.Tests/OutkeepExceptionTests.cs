1c76e6ac2fe2e7ab757865f525a89191daf0374a
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-04 23:51:02 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/OutkeepExceptionTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class OutkeepExceptionTests
+    {
+        [Fact]
+        public void Constructs()
+        {
+            // arrange
+            var exception = new OutkeepException();
+
+            // assert
+            Assert.NotNull(exception.Message);
+            Assert.Null(exception.InnerException);
+        }
+    }
+}
