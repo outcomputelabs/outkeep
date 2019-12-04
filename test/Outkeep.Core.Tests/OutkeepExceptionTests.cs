7904d2774690906be4cc284163bda576d3c1ef86
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-04 23:57:29 +0000

    Added test

--- test/Outkeep.Core.Tests/OutkeepExceptionTests.cs
+++ test/Outkeep.Core.Tests/OutkeepExceptionTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using Xunit;
 
 namespace Outkeep.Core.Tests
@@ -39,6 +41,31 @@ namespace Outkeep.Core.Tests
             // assert
             Assert.Equal(message, exception.Message);
             Assert.Same(inner, exception.InnerException);
+        }
+
+        [Fact]
+        public void CanSerialize()
+        {
+            // act
+            var message = "Some Message";
+            var inner = new Exception("Some Inner Exception");
+            var exception = new OutkeepException(message, inner);
+            var formatter = new BinaryFormatter();
+
+            // act
+            object obj;
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, exception);
+                stream.Position = 0;
+                obj = formatter.Deserialize(stream);
+            }
+
+            // assert
+            Assert.NotNull(obj);
+            var result = Assert.IsType<OutkeepException>(obj);
+            Assert.Equal(inner.Message, result.InnerException.Message);
+            Assert.Equal(message, result.Message);
         }
     }
 }
