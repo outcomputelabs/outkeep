fba74576a62754781215132a3b13b1a99fd73108
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-04 19:02:00 +0000

    Added test

--- test/Outkeep.Hosting.Tests/TcpHelperTests.cs
+++ test/Outkeep.Hosting.Tests/TcpHelperTests.cs
@@ -1,12 +1,20 @@
-﻿using Xunit;
+﻿using Moq;
+using System;
+using Xunit;
 
 namespace Outkeep.Hosting.Tests
 {
     public class TcpHelperTests
     {
         [Fact]
-        public void FindsFirstFreePort()
+        public void RefusesLowPort()
         {
+            // arrange
+            var factory = Mock.Of<ITcpListenerWrapperFactory>();
+            var helper = new TcpHelper(factory);
+
+            // act and assert
+            Assert.Throws<ArgumentOutOfRangeException>("start", () => helper.GetFreePort(0, 60000));
         }
     }
 }
