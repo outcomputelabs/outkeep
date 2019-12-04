8f9334937eaf924b347dc55e5c01095acc04f94a
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-04 20:19:03 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/FileCacheStorageExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class FileCacheStorageExceptionTests
+    {
+        [Fact]
+        public void Constructs()
+        {
+            // act
+            var exception = new FileCacheStorageException();
+
+            // assert
+            Assert.Null(exception.Path);
+            Assert.Null(exception.Key);
+            Assert.Null(exception.OtherKey);
+            Assert.NotNull(exception.Message);
+        }
+    }
+}
