b160b8601655636c9da8a49bb7ac5ed0c315b2d1
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-05 10:03:27 +0000

    Added test

--- src/Outkeep.Hosting/ServiceCollectionHostingExtensions.cs
+++ src/Outkeep.Hosting/ServiceCollectionHostingExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class ServiceCollectionExtensions
+    public static class ServiceCollectionHostingExtensions
     {
         public static IServiceCollection AddOutkeepServer(this IServiceCollection services)
         {
---
+++ test/Outkeep.Hosting.Tests/ServiceCollectionHostingExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using System.Linq;
+using Xunit;
+
+namespace Outkeep.Hosting.Tests
+{
+    public class ServiceCollectionHostingExtensionsTests
+    {
+        [Fact]
+        public void AddOutkeepServer()
+        {
+            // arrange
+            var services = Mock.Of<IServiceCollection>();
+            Mock.Get(services).Setup(x => x.GetEnumerator()).Returns(Enumerable.Empty<ServiceDescriptor>().GetEnumerator());
+            Mock.Get(services).Setup(x => x.Add(It.Is<ServiceDescriptor>(v => v.ServiceType == typeof(IHostedService) && v.ImplementationType == typeof(OutkeepServerHostedService) && v.Lifetime == ServiceLifetime.Singleton)));
+
+            // act
+            var result = services.AddOutkeepServer();
+
+            // assert
+            Assert.Same(services, result);
+            Mock.Get(services).VerifyAll();
+            Mock.Get(services).VerifyNoOtherCalls();
+        }
+    }
+}
