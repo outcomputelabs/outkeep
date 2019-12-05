04fdfa6c2e137fa64451dca6d8b0ebb2394385d9
Jorge Candeias <candeias@fastmail.fm>
2019-12-05 22:04:46 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/StorageExceptionTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class StorageExceptionTests
+    {
+        [Fact]
+        public void Constructs()
+        {
+            // act
+            var exception = new StorageException();
+
+            // assert
+            Assert.NotNull(exception.Message);
+            Assert.Null(exception.InnerException);
+        }
+    }
+}
