e01cae8d1f1079b7c9bbc184958dc0ee737149ce
Jorge Candeias <candeias@fastmail.fm>
2019-12-05 21:52:33 +0000

    Added test

---
+++ test/Outkeep.Client.Tests/OutkeepClientBuilderExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using System;
+using Xunit;
+
+namespace Outkeep.Client.Tests
+{
+    public class OutkeepClientBuilderExtensionsTests
+    {
+        [Fact]
+        public void ConfigureServicesWithoutContext()
+        {
+            // arrange
+            var registered = false;
+            var builder = Mock.Of<IOutkeepClientBuilder>();
+            Mock.Get(builder)
+                .Setup(x => x.ConfigureServices(It.IsAny<Action<HostBuilderContext, IServiceCollection>>()))
+                .Callback((Action<HostBuilderContext, IServiceCollection> action) => action(null, null))
+                .Returns(builder);
+
+            // act
+            var result = OutkeepClientBuilderExtensions.ConfigureServices(builder, services => { registered = true; });
+
+            // assert
+            Assert.Same(builder, result);
+            Assert.True(registered);
+            Mock.Get(builder).VerifyAll();
+        }
+    }
+}
