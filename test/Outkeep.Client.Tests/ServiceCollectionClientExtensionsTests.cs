a89cc791b946868d85804d95ff980f18fbee56e3
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-05 00:40:07 +0000

    Added test

--- test/Outkeep.Client.Tests/ServiceCollectionClientExtensionsTests.cs
+++ test/Outkeep.Client.Tests/ServiceCollectionClientExtensionsTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Moq;
+using System.Linq;
 using Xunit;
 
 namespace Outkeep.Client.Tests
@@ -16,6 +18,23 @@ namespace Outkeep.Client.Tests
 
             // act
             var result = services.AddOutkeepDistributedCache();
+
+            // assert
+            Assert.Same(services, result);
+            Mock.Get(services).VerifyAll();
+            Mock.Get(services).VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void AddOutkeepClient()
+        {
+            // arrange
+            var services = Mock.Of<IServiceCollection>();
+            Mock.Get(services).Setup(x => x.GetEnumerator()).Returns(Enumerable.Empty<ServiceDescriptor>().GetEnumerator());
+            Mock.Get(services).Setup(x => x.Add(It.Is<ServiceDescriptor>(v => v.ServiceType == typeof(IHostedService) && v.ImplementationType == typeof(OutkeepClientHostedService) && v.Lifetime == ServiceLifetime.Singleton)));
+
+            // act
+            var result = services.AddOutkeepClient();
 
             // assert
             Assert.Same(services, result);
