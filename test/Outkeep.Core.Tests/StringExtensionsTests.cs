f7c65d74ef128a035f7991a482b3cf421a45c872
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-05 00:45:26 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/StringExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void FormatsOneArgument()
+        {
+            // arrange
+            var arg0 = "Zero";
+            var format = "Arg {0}";
+            var expected = string.Format(CultureInfo.InvariantCulture, format, arg0);
+
+            // act
+            var result = format.Format(arg0);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
