7b17bf70b711a8ac6cf1e06a855678552a7f1ff2
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-06 18:02:43 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/NullTimerEntryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class NullTimerEntryTests
+    {
+        [Fact]
+        public void SetsDisposedOnDisposed()
+        {
+            // arrange
+            var entry = new NullTimerEntry(_ => Task.CompletedTask, null, TimeSpan.Zero, TimeSpan.Zero);
+
+            // assert
+            Assert.False(entry.IsDisposed);
+
+            // act
+            entry.Dispose();
+
+            // assert
+            Assert.True(entry.IsDisposed);
+        }
+    }
+}
