d8a0c74673ee1ebd51ecb679f423fef987dda3a3
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-23 14:39:50 +0000

    Cleanup

--- src/Outkeep.Core/Caching/CacheEntryExtensions.cs
+++ src/Outkeep.Core/Caching/CacheEntryExtensions.cs
@@ -26,11 +26,10 @@ namespace Outkeep.Core.Caching
         /// <summary>
         /// Sets the sliding expiration for this cache entry.
         /// </summary>
-        /// <typeparam name="TKey">The type of key of this cache entry.</typeparam>
         /// <param name="entry">The cache entry instance upon which to set the sliding expiration.</param>
         /// <param name="slidingExpiration">The sliding expiration to set upon the cache entry.</param>
         /// <returns>The same cache entry instance to allow chaining.</returns>
-        public static ICacheEntry SetSlidingExpiration<TKey>(this ICacheEntry entry, TimeSpan slidingExpiration)
+        public static ICacheEntry SetSlidingExpiration(this ICacheEntry entry, TimeSpan slidingExpiration)
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
@@ -42,11 +41,10 @@ namespace Outkeep.Core.Caching
         /// <summary>
         /// Sets the priority for this cache entry.
         /// </summary>
-        /// <typeparam name="TKey">The type of key of this cache entry.</typeparam>
         /// <param name="entry">The cache entry instance upon which to set the priority.</param>
         /// <param name="priority">The priority to set upon the cache entry.</param>
         /// <returns>The same cache entry instance to allow chaining.</returns>
-        public static ICacheEntry SetPriority<TKey>(this ICacheEntry entry, CachePriority priority)
+        public static ICacheEntry SetPriority(this ICacheEntry entry, CachePriority priority)
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
