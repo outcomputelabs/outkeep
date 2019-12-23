d09051939bca8bf0e9807cacbed19c604e891c08
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-23 14:05:13 +0000

    Cleanup

--- src/Outkeep.Client/LoggerExtensions.cs
+++ src/Outkeep.Client/LoggerExtensions.cs
@@ -4,12 +4,12 @@ namespace Microsoft.Extensions.Logging
 {
     internal static class LoggerExtensions
     {
-        private static readonly Action<ILogger, Exception> OutkeepClientStarted = LoggerMessage.Define(
+        private static readonly Action<ILogger, Exception?> OutkeepClientStarted = LoggerMessage.Define(
             LogLevel.Information, new EventId(0, nameof(OutkeepClientStarted)), "Outkeep Client Started");
 
         public static void LogOutkeepClientStarted(this ILogger logger) => OutkeepClientStarted(logger, null);
 
-        private static readonly Action<ILogger, Exception> OutkeepClientStopped = LoggerMessage.Define(
+        private static readonly Action<ILogger, Exception?> OutkeepClientStopped = LoggerMessage.Define(
             LogLevel.Information, new EventId(0, nameof(OutkeepClientStopped)), "Outkeep Client Stopped");
 
         public static void LogOutkeepClientStopped(this ILogger logger) => OutkeepClientStopped(logger, null);
