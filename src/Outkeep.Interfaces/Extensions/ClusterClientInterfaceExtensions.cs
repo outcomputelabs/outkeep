8533592295b77d3312664eceb90266bd2b8c8094
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-23 12:01:29 +0000

    Refactored extensions

--- src/Outkeep.Interfaces/Extensions/ClusterClientInterfaceExtensions.cs
+++ src/Outkeep.Interfaces/Extensions/ClusterClientInterfaceExtensions.cs
@@ -1,29 +1,22 @@
 ﻿using Outkeep.Interfaces;
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Orleans
 {
     public static class ClusterClientInterfaceExtensions
     {
-        [SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
         public static ICacheGrain GetCacheGrain(this IClusterClient client, string key)
         {
-            if (client == null) ThrowClientNull();
+            if (client == null) throw new ArgumentNullException(nameof(client));
 
             return client.GetGrain<ICacheGrain>(key);
-
-            void ThrowClientNull() => throw new ArgumentNullException(nameof(client));
         }
 
-        [SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
         public static IEchoGrain GetEchoGrain(this IClusterClient client)
         {
-            if (client == null) ThrowClientNull();
+            if (client == null) throw new ArgumentNullException(nameof(client));
 
             return client.GetGrain<IEchoGrain>(Guid.Empty);
-
-            void ThrowClientNull() => throw new ArgumentNullException(nameof(client));
         }
     }
 }
