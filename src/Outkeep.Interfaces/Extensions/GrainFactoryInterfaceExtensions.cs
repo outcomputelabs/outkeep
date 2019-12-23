21b7da580cdf654137ec51dc73de7f12f7c6aa6d
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-23 12:02:54 +0000

    Refactored extensions

--- src/Outkeep.Interfaces/Extensions/GrainFactoryInterfaceExtensions.cs
+++ src/Outkeep.Interfaces/Extensions/GrainFactoryInterfaceExtensions.cs
@@ -1,31 +1,23 @@
 ﻿using Outkeep.Interfaces;
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Orleans
 {
     public static class GrainFactoryInterfaceExtensions
     {
-        [SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
         public static ICacheGrain GetCacheGrain(this IGrainFactory factory, string key)
         {
-            if (factory == null) ThrowFactoryNull();
-            if (key == null) ThrowKeyNull();
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
+            if (key == null) throw new ArgumentNullException(nameof(key));
 
             return factory.GetGrain<ICacheGrain>(key);
-
-            void ThrowFactoryNull() => throw new ArgumentNullException(nameof(factory));
-            void ThrowKeyNull() => throw new ArgumentNullException(nameof(key));
         }
 
-        [SuppressMessage("Design", "CA1062:Validate arguments of public methods")]
         public static IEchoGrain GetEchoGrain(this IGrainFactory factory)
         {
-            if (factory == null) ThrowFactoryNull();
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
 
             return factory.GetGrain<IEchoGrain>(Guid.Empty);
-
-            void ThrowFactoryNull() => throw new ArgumentNullException(nameof(factory));
         }
     }
 }
