5e410b9068b85ff80b4f7283af41112701fddb52
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-23 08:30:57 +0000

    Cleanup

--- src/Outkeep.Core/Caching/OvercapacityCallbackRegistration.cs
+++ src/Outkeep.Core/Caching/OvercapacityCallbackRegistration.cs
@@ -4,14 +4,37 @@ using System.Threading.Tasks;
 
 namespace Outkeep.Core.Caching
 {
+    /// <summary>
+    /// Holds a callback registration for overcapacity notifications.
+    /// Implements the Disposable Action pattern to notify the appropriate context of disposal.
+    /// This class is thread-safe.
+    /// </summary>
     internal sealed class OvercapacityCallbackRegistration : IDisposable
     {
+        /// <summary>
+        /// The callback to invoke.
+        /// </summary>
         private Action<object?>? _callback;
-        private readonly object? _state;
-        private readonly TaskScheduler _scheduler;
-        private readonly ICacheContext _context;
-        private bool _disposed;
 
+        /// <summary>
+        /// The state to pass onto the callback.
+        /// </summary>
+        private readonly object? _state;
+
+        /// <summary>
+        /// The task scheduler to schedule the callback on.
+        /// This allows callbacks to execute on grain activation task schedulers.
+        /// </summary>
+        private readonly TaskScheduler _scheduler;
+
+        /// <summary>
+        /// The cache context to notify upon disposal of this registration.
+        /// </summary>
+        private readonly ICacheContext _context;
+
+        /// <summary>
+        /// Creates a new <see cref="OvercapacityCallbackRegistration"/> instance.
+        /// </summary>
         public OvercapacityCallbackRegistration(Action<object?> callback, object? state, TaskScheduler scheduler, ICacheContext context)
         {
             _callback = callback ?? throw new ArgumentNullException(nameof(callback));
@@ -20,6 +43,9 @@ namespace Outkeep.Core.Caching
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+        /// <summary>
+        /// Schedules the callback on the given task scheduler.
+        /// </summary>
         public void Schedule()
         {
             // capture for thread-safety
@@ -37,19 +63,29 @@ namespace Outkeep.Core.Caching
                 _scheduler);
         }
 
+        #region Disposable
+
+        private bool _disposed;
+
         public void Dispose()
         {
+            if (_disposed) return;
+
             if (Interlocked.Exchange(ref _callback, null) != null)
             {
                 _context.OnOvercapacityCallbackRegistrationDisposed(this);
             }
 
             GC.SuppressFinalize(this);
+
+            _disposed = true;
         }
 
         ~OvercapacityCallbackRegistration()
         {
             Dispose();
         }
+
+        #endregion Disposable
     }
 }
