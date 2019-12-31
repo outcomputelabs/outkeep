67953342e8ab52685745315fd7d483f312981c30
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-31 08:28:57 +0000

    Cleanup

--- src/Outkeep.Interfaces/ICacheDirectorGrainService.cs
+++ src/Outkeep.Interfaces/ICacheDirectorGrainService.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace Outkeep.Interfaces
 {
+    /// <summary>
+    /// Coordinates capacity claims between cache grains within a single host.
+    /// </summary>
     public interface ICacheDirectorGrainService : IGrainService
     {
         Task PingAsync();
