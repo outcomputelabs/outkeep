37c322efe8594cad14580df1f7ddc98d2097add1
Jorge Candeias <jorge.candeias@outcompute.com>
2019-12-31 08:30:02 +0000

    Added comments

--- src/Outkeep.Interfaces/ICacheDirectorGrainServiceClient.cs
+++ src/Outkeep.Interfaces/ICacheDirectorGrainServiceClient.cs
@@ -2,6 +2,9 @@
 
 namespace Outkeep.Interfaces
 {
+    /// <summary>
+    /// Client for <see cref="ICacheDirectorGrainService"/>.
+    /// </summary>
     public interface ICacheDirectorGrainServiceClient : IGrainServiceClient<ICacheDirectorGrainService>, ICacheDirectorGrainService
     {
     }
