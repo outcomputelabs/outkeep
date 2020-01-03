7894caad8e1b023cb47cbe3965946ec2a630f5ef
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-03 14:07:16 +0000

    Moved extensions

--- src/Outkeep.Core/OutkeepServerBuilderExtensions.cs
+++ src/Outkeep.Core/OutkeepServerBuilderExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Orleans.Hosting;
 using System;
+using HostBuilderContext = Microsoft.Extensions.Hosting.HostBuilderContext;
 
 namespace Outkeep.Core
 {
@@ -55,6 +56,23 @@ namespace Outkeep.Core
             return builder.ConfigureServices((context, services) =>
             {
                 services.TryAddSingleton<ICacheStorage, MemoryCacheStorage>();
+            });
+        }
+
+        /// <inheritdoc cref="OptionsServiceCollectionExtensions.Configure{TOptions}(IServiceCollection, Action{TOptions})"/>
+        public static IOutkeepServerBuilder Configure<TOptions>(this IOutkeepServerBuilder builder, Action<TOptions> configure) where TOptions : class
+        {
+            return builder.Configure<TOptions>((context, options) => configure(options));
+        }
+
+        /// <inheritdoc cref="OptionsServiceCollectionExtensions.Configure{TOptions}(IServiceCollection, Action{TOptions})"/>
+        public static IOutkeepServerBuilder Configure<TOptions>(this IOutkeepServerBuilder builder, Action<HostBuilderContext, TOptions> configure) where TOptions : class
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            return builder.ConfigureServices((context, services) =>
+            {
+                services.Configure<TOptions>(options => configure(context, options));
             });
         }
     }
