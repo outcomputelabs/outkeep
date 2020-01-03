c84855b3f8cce845b54f930b382865467baed415
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-03 14:10:52 +0000

    Added extensions

---
+++ src/Outkeep.Core/Caching/HostBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Outkeep.Core.Caching
+{
+    public static class HostBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="CacheDirector"/> to the <see cref="IHostBuilder"/> as an <see cref="ICacheDirector"/>.
+        /// </summary>
+        public static IHostBuilder UseCacheDirector(this IHostBuilder builder, Action<HostBuilderContext, CacheDirectorOptions> configure)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            return builder.ConfigureServices((context, services) =>
+            {
+                services.AddCacheDirector(options => configure(context, options));
+            });
+        }
+    }
+}
