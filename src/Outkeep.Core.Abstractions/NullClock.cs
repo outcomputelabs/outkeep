24223081265fc23c1e4ba93dda2dfb814eca8f7d
Jorge Candeias <candeias@fastmail.fm>
2020-01-05 03:08:10 +0000

    Added test

---
+++ src/Outkeep.Core.Abstractions/NullClock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Outkeep.Core
+{
+    public class NullClock : ISystemClock
+    {
+        public DateTimeOffset UtcNow => DateTimeOffset.MinValue;
+
+        public static NullClock Instance => new NullClock();
+    }
+}
---
+++ test/Outkeep.Core.Tests/CacheDirectorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Outkeep.Core.Caching;
+using System;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class CacheDirectorTests
+    {
+        [Fact]
+        public void Initializes()
+        {
+            // arrange
+            var options = new CacheDirectorOptions
+            {
+                AutomaticOvercapacityCompaction = true,
+                ExpirationScanFrequency = TimeSpan.FromMinutes(1),
+                OvercapacityCompactionFrequency = TimeSpan.FromMinutes(1),
+                MaxCapacity = 10000,
+                TargetCapacity = 8000
+            };
+
+            // act
+            var director = new CacheDirector(Options.Create(options), NullLogger<CacheDirector>.Instance, NullClock.Instance);
+
+            // assert
+            Assert.Equal(0, director.Count);
+            Assert.Equal(0, director.Size);
+            Assert.Equal(options.MaxCapacity, director.MaxCapacity);
+            Assert.Equal(options.TargetCapacity, director.TargetCapacity);
+        }
+    }
+}
