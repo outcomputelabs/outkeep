92fd871551fe5fd4852a6611bf43a58f4843fc84
Jorge Candeias <candeias@fastmail.fm>
2020-01-06 01:20:32 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/HostBuilderExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Outkeep.Core.Caching;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class HostBuilderExtensionsTests
+    {
+        [Fact]
+        public void UseCacheDirector()
+        {
+            // arrange
+            var builder = new HostBuilder();
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<ISystemClock>(NullClock.Instance);
+            });
+
+            // act
+            builder.UseCacheDirector((context, options) =>
+            {
+                options.MaxCapacity = 10000;
+                options.TargetCapacity = 8000;
+            });
+
+            // assert
+            var services = builder.Build().Services;
+            var director = services.GetRequiredService<ICacheDirector>();
+            Assert.NotNull(director);
+            Assert.Equal(10000, director.MaxCapacity);
+            Assert.Equal(8000, director.TargetCapacity);
+        }
+    }
+}
