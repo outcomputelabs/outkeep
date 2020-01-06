433e64959bb89bed151e6504de836767b3dd57f3
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-06 18:25:20 +0000

    Moved logging methods

--- src/Outkeep.Hosting/OutkeepServerHostedService.cs
+++ src/Outkeep.Hosting/OutkeepServerHostedService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Outkeep.Hosting.Properties;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,25 +9,80 @@ namespace Outkeep.Hosting
 {
     internal class OutkeepServerHostedService : IHostedService
     {
-        private readonly ILogger<OutkeepServerHostedService> logger;
+        private readonly ILogger _logger;
 
         public OutkeepServerHostedService(ILogger<OutkeepServerHostedService> logger)
         {
-            this.logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            logger.LogOutkeepServerStarting();
-            logger.LogOutkeepServerStarted();
+            Log.OutkeepServerStarting(_logger);
+            Log.OutkeepServerStarted(_logger);
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            logger.LogOutkeepServerStopping();
-            logger.LogOutkeepServerStopped();
+            Log.OutkeepServerStopping(_logger);
+            Log.OutkeepServerStopped(_logger);
             return Task.CompletedTask;
+        }
+
+        private static class Log
+        {
+            #region OutkeepServerStarting
+
+            private static readonly Action<ILogger, Exception?> OutkeepServerStartingAction =
+                LoggerMessage.Define(
+                    LogLevel.Information,
+                    new EventId(0, nameof(OutkeepServerStarting)),
+                    Resources.LogOutkeepServerStarting);
+
+            public static void OutkeepServerStarting(ILogger logger) =>
+                OutkeepServerStartingAction(logger, null);
+
+            #endregion OutkeepServerStarting
+
+            #region OutkeepServerStarted
+
+            private static readonly Action<ILogger, Exception?> OutkeepServerStartedAction =
+                LoggerMessage.Define(
+                    LogLevel.Information,
+                    new EventId(0, nameof(OutkeepServerStarted)),
+                    Resources.LogOutkeepServerStarted);
+
+            public static void OutkeepServerStarted(ILogger logger) =>
+                OutkeepServerStartedAction(logger, null);
+
+            #endregion OutkeepServerStarted
+
+            #region OutkeepServerStopping
+
+            private static readonly Action<ILogger, Exception?> OutkeepServerStoppingAction =
+                LoggerMessage.Define(
+                    LogLevel.Information,
+                    new EventId(0, nameof(OutkeepServerStopping)),
+                    Resources.LogOutkeepServerStopping);
+
+            public static void OutkeepServerStopping(ILogger logger) =>
+                OutkeepServerStoppingAction(logger, null);
+
+            #endregion OutkeepServerStopping
+
+            #region OutkeepServerStopped
+
+            private static readonly Action<ILogger, Exception?> OutkeepServerStoppedAction =
+                LoggerMessage.Define(
+                    LogLevel.Information,
+                    new EventId(0, nameof(OutkeepServerStopped)),
+                    Resources.LogOutkeepServerStopped);
+
+            public static void OutkeepServerStopped(ILogger logger) =>
+                OutkeepServerStoppedAction(logger, null);
+
+            #endregion OutkeepServerStopped
         }
     }
 }
