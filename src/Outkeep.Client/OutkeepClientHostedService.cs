d7530814c1968f89829bdb02e6251372351eaf98
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-06 18:31:51 +0000

    Moved logging methods

--- src/Outkeep.Client/OutkeepClientHostedService.cs
+++ src/Outkeep.Client/OutkeepClientHostedService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Outkeep.Client.Properties;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,25 +9,54 @@ namespace Outkeep.Client
 {
     internal class OutkeepClientHostedService : IHostedService
     {
-        private readonly ILogger<OutkeepClientHostedService> logger;
+        private readonly ILogger<OutkeepClientHostedService> _logger;
 
         public OutkeepClientHostedService(ILogger<OutkeepClientHostedService> logger)
         {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            logger.LogOutkeepClientStarted();
+            Log.OutkeepClientStarted(_logger);
 
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            logger.LogOutkeepClientStopped();
+            Log.OutkeepClientStopped(_logger);
 
             return Task.CompletedTask;
+        }
+
+        private static class Log
+        {
+            #region OutkeepClientStarted
+
+            private static readonly Action<ILogger, Exception?> OutkeepClientStartedAction =
+                LoggerMessage.Define(
+                    LogLevel.Information,
+                    new EventId(0, nameof(OutkeepClientStarted)),
+                    Resources.Log_OutkeepClientStarted);
+
+            public static void OutkeepClientStarted(ILogger logger) =>
+                OutkeepClientStartedAction(logger, null);
+
+            #endregion OutkeepClientStarted
+
+            #region OutkeepClientStopped
+
+            private static readonly Action<ILogger, Exception?> OutkeepClientStoppedAction =
+                LoggerMessage.Define(
+                    LogLevel.Information,
+                    new EventId(0, nameof(OutkeepClientStopped)),
+                    Resources.Log_OutkeepClientStopped);
+
+            public static void OutkeepClientStopped(ILogger logger) =>
+                OutkeepClientStoppedAction(logger, null);
+
+            #endregion OutkeepClientStopped
         }
     }
 }
