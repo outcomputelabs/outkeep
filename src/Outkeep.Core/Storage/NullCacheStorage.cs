20394ebf9994aaf9608c467f5941afccbea02f83
Jorge Candeias <candeias@fastmail.fm>
2020-01-07 22:40:55 +0000

    Added NullCacheStorage

---
+++ src/Outkeep.Core/Storage/NullCacheStorage.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Outkeep.Core.Storage
+{
+    /// <summary>
+    /// Implements a <see cref="ICacheStorage"/> provider that does not store anything.
+    /// Use this to disable cache storage altogether.
+    /// </summary>
+    public class NullCacheStorage : ICacheStorage
+    {
+        private static readonly Task<CacheItem?> NullTask = Task.FromResult<CacheItem?>(null);
+
+        public Task ClearAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<CacheItem?> ReadAsync(string key, CancellationToken cancellationToken = default)
+        {
+            return NullTask;
+        }
+
+        public Task WriteAsync(string key, CacheItem item, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
