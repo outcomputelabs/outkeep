c6fc22f730bb1d9814b06a57393385b2b61dfc46
Jorge Candeias <candeias@fastmail.fm>
2020-01-08 23:40:55 +0000

    Added tests

---
+++ test/Outkeep.Core.Tests/ConcurrentDictionaryExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class ConcurrentDictionaryExtensionsTests
+    {
+        [Fact]
+        public void TryRemoveRemovesMatchingEntry()
+        {
+            // arrange
+            var dictionary = new ConcurrentDictionary<Guid, Guid>();
+            var first = Guid.NewGuid();
+            var second = Guid.NewGuid();
+            dictionary[first] = first;
+            dictionary[second] = second;
+
+            // act
+            var result = dictionary.TryRemove(new KeyValuePair<Guid, Guid>(first, first));
+
+            // assert
+            Assert.True(result);
+            Assert.Single(dictionary);
+            Assert.True(dictionary.ContainsKey(second));
+        }
+
+        [Fact]
+        public void TryRemoveDoesNotRemovesNonMatchingEntry()
+        {
+            // arrange
+            var dictionary = new ConcurrentDictionary<Guid, Guid>();
+            var first = Guid.NewGuid();
+            var second = Guid.NewGuid();
+            dictionary[first] = first;
+            dictionary[second] = second;
+
+            // act
+            var result = dictionary.TryRemove(new KeyValuePair<Guid, Guid>(first, second));
+
+            // assert
+            Assert.False(result);
+            Assert.Equal(2, dictionary.Count);
+            Assert.True(dictionary.ContainsKey(first));
+            Assert.True(dictionary.ContainsKey(second));
+        }
+    }
+}
