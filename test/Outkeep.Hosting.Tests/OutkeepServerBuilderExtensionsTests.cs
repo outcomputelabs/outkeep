3fad0db3eac006268d95d1e336dea0a31bac3ee4
Jorge Candeias <candeias@fastmail.fm>
2020-01-09 20:29:46 +0000

    Removed duplications

--- test/Outkeep.Hosting.Tests/OutkeepServerBuilderExtensionsTests.cs
+++ test/Outkeep.Hosting.Tests/OutkeepServerBuilderExtensionsTests.cs
@@ -1,14 +1,43 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Moq;
 using Orleans.Hosting;
 using System;
 using Xunit;
 using HostBuilderContext = Microsoft.Extensions.Hosting.HostBuilderContext;
 
-namespace Outkeep.Core.Tests
+namespace Outkeep.Hosting.Tests
 {
     public class OutkeepServerBuilderExtensionsTests
     {
+        private class MyOptions
+        {
+            public int SomeProperty { get; set; }
+        }
+
+        [Fact]
+        public void ConfiguresOptions()
+        {
+            var services = new ServiceCollection();
+
+            var builder = Mock.Of<IOutkeepServerBuilder>();
+            Mock.Get(builder)
+                .Setup(x => x.ConfigureServices(It.IsAny<Action<HostBuilderContext, IServiceCollection>>()))
+                .Callback((Action<HostBuilderContext, IServiceCollection> action) => action(null!, services))
+                .Returns(builder);
+
+            builder.Configure<MyOptions>(options =>
+            {
+                options.SomeProperty = 123;
+            });
+
+            var options = services.BuildServiceProvider().GetService<IOptions<MyOptions>>();
+            Assert.NotNull(options);
+            Assert.Equal(123, options.Value.SomeProperty);
+
+            Mock.Get(builder).VerifyNoOtherCalls();
+        }
+
         [Fact]
         public void ConfigureServicesWithServiceCollectionOnly()
         {
