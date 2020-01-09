7a408ac1397241ba34a03e50b0083cdfd1802c37
Jorge Candeias <candeias@fastmail.fm>
2020-01-09 00:02:37 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/NullCacheStorageTests.cs
@@ -0,0 +1,27 @@
+﻿using Outkeep.Core.Storage;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class NullCacheStorageTests
+    {
+        [Fact]
+        public async Task Cycles()
+        {
+            // arrange
+            var service = new NullCacheStorage();
+            var key = "SomeKey";
+            var value = Guid.NewGuid().ToByteArray();
+
+            // act
+            await service.WriteAsync(key, new CacheItem(value, null, null)).ConfigureAwait(false);
+            var result = await service.ReadAsync(key).ConfigureAwait(false);
+            await service.ClearAsync(key).ConfigureAwait(false);
+
+            // assert
+            Assert.Null(result);
+        }
+    }
+}
