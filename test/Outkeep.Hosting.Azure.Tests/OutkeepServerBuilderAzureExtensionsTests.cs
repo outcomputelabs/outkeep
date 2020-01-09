21152f7eb2f1f627f7842e258eedd2454fff59d5
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-09 09:47:50 +0000

    Added placeholder test

---
+++ test/Outkeep.Hosting.Azure.Tests/OutkeepServerBuilderAzureExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Outkeep.Hosting.Azure.Tests
+{
+    public class OutkeepServerBuilderAzureExtensionsTests
+    {
+        [Fact]
+        public void UseAzure()
+        {
+            // arrange
+            var builder = new HostBuilder();
+
+            // act
+            builder.UseOutkeepServer(outkeep =>
+            {
+                outkeep.UseAzure();
+            });
+
+            // assert
+            Assert.True(true);
+        }
+    }
+}
