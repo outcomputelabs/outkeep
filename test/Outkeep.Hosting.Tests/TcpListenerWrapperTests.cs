45f6131b0da5a1e1f171adeacefa767907f4063c
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-09 15:13:54 +0000

    Added test

---
+++ test/Outkeep.Hosting.Tests/TcpListenerWrapperTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Xunit;
+
+namespace Outkeep.Hosting.Tests
+{
+    public class TcpListenerWrapperTests
+    {
+        [Fact]
+        public void Cycles()
+        {
+            // arrange
+            var listener = TcpListener.Create(0);
+            listener.ExclusiveAddressUse = true;
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            // act
+            var wrapper = new TcpListenerWrapper(port, true);
+
+            // assert
+            var endpoint = Assert.IsType<IPEndPoint>(wrapper.LocalEndpoint);
+            Assert.Equal(port, endpoint.Port);
+
+            // act
+            wrapper.Start();
+            wrapper.Stop();
+        }
+    }
+}
