31c5e4118bdf75ed300315c695d633d4b0f4e78f
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-09 10:06:19 +0000

    Added test

---
+++ test/Outkeep.Core.Tests/OutkeepServerBuilderExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using System;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class OutkeepServerBuilderExtensionsTests
+    {
+        [Fact]
+        public void ConfigureServicesWithServiceCollectionOnly()
+        {
+            // arrange
+            var builder = Mock.Of<IOutkeepServerBuilder>();
+            var services = Mock.Of<IServiceCollection>();
+            Action<HostBuilderContext, IServiceCollection>? action = null;
+            Mock.Get(builder)
+                .Setup(x => x.ConfigureServices(It.IsAny<Action<HostBuilderContext, IServiceCollection>>()))
+                .Callback((Action<HostBuilderContext, IServiceCollection> a) => { action = a; })
+                .Returns(builder);
+
+            // act
+            IServiceCollection? input = null;
+            var result = builder.ConfigureServices(services =>
+            {
+                input = services;
+            });
+
+            // assert
+            Assert.Same(builder, result);
+            Assert.NotNull(action);
+            action?.Invoke(null!, services);
+            Assert.NotNull(input);
+            Assert.Same(services, input);
+        }
+    }
+}
