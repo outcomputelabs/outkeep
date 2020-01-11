5c4004e1c261a5e878295a577a0273bcac947d30
Jorge Candeias <candeias@fastmail.fm>
2020-01-11 18:27:44 +0000

    Added test

--- test/Outkeep.Hosting.Tests/HostBuilderExtensionsTests.cs
+++ test/Outkeep.Hosting.Tests/HostBuilderExtensionsTests.cs
@@ -23,5 +23,24 @@ namespace Outkeep.Hosting.Tests
             Assert.Same(builder, result);
             Assert.True(called);
         }
+
+        [Fact]
+        public void UseOutkeepServerWithoutContextCallsActionsOnBuild()
+        {
+            var builder = new HostBuilder();
+            var called = false;
+
+            var result = builder.UseOutkeepServer(outkeep =>
+            {
+                called = true;
+            });
+
+            Assert.False(called);
+
+            builder.Build();
+
+            Assert.Same(builder, result);
+            Assert.True(called);
+        }
     }
 }
