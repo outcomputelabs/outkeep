62e634b4c2f037d0f1ebdd5696e70e025c06b1b4
Jorge Candeias <candeias@fastmail.fm>
2020-01-16 22:25:48 +0000

    Added type constraints

--- src/Outkeep.Core.Abstractions/Caching/CacheEntryExtensions.cs
+++ src/Outkeep.Core.Abstractions/Caching/CacheEntryExtensions.cs
@@ -15,7 +15,7 @@ namespace Outkeep.Core.Caching
         /// <param name="entry">The cache entry.</param>
         /// <param name="utcLastAccessed">The value for the <see cref="ICacheEntry.UtcLastAccessed"/> property.</param>
         /// <returns>The same cache entry instance to allow chaining.</returns>
-        public static ICacheEntry<TKey> SetUtcLastAccessed<TKey>(this ICacheEntry<TKey> entry, DateTimeOffset utcLastAccessed)
+        public static ICacheEntry<TKey> SetUtcLastAccessed<TKey>(this ICacheEntry<TKey> entry, DateTimeOffset utcLastAccessed) where TKey : notnull
         {
             if (entry is null) throw new ArgumentNullException(nameof(entry));
 
@@ -30,7 +30,7 @@ namespace Outkeep.Core.Caching
         /// <param name="entry">The cache entry instance upon which to set the absolute expiration.</param>
         /// <param name="absoluteExpiration">The absolute expiration to set upon the cache entry.</param>
         /// <returns>The same cache entry instance to allow chaining.</returns>
-        public static ICacheEntry<TKey> SetAbsoluteExpiration<TKey>(this ICacheEntry<TKey> entry, DateTimeOffset? absoluteExpiration)
+        public static ICacheEntry<TKey> SetAbsoluteExpiration<TKey>(this ICacheEntry<TKey> entry, DateTimeOffset? absoluteExpiration) where TKey : notnull
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
@@ -45,7 +45,7 @@ namespace Outkeep.Core.Caching
         /// <param name="entry">The cache entry instance upon which to set the sliding expiration.</param>
         /// <param name="slidingExpiration">The sliding expiration to set upon the cache entry.</param>
         /// <returns>The same cache entry instance to allow chaining.</returns>
-        public static ICacheEntry<TKey> SetSlidingExpiration<TKey>(this ICacheEntry<TKey> entry, TimeSpan? slidingExpiration)
+        public static ICacheEntry<TKey> SetSlidingExpiration<TKey>(this ICacheEntry<TKey> entry, TimeSpan? slidingExpiration) where TKey : notnull
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
@@ -60,7 +60,7 @@ namespace Outkeep.Core.Caching
         /// <param name="entry">The cache entry instance upon which to set the priority.</param>
         /// <param name="priority">The priority to set upon the cache entry.</param>
         /// <returns>The same cache entry instance to allow chaining.</returns>
-        public static ICacheEntry<TKey> SetPriority<TKey>(this ICacheEntry<TKey> entry, CachePriority priority)
+        public static ICacheEntry<TKey> SetPriority<TKey>(this ICacheEntry<TKey> entry, CachePriority priority) where TKey : notnull
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
@@ -72,7 +72,7 @@ namespace Outkeep.Core.Caching
         /// <summary>
         /// Convenience method to schedule a continuation on the <see cref="ICacheEntry.Evicted"/> property using the current task scheduler.
         /// </summary>
-        public static ICacheEntry<TKey> ContinueWithOnEvicted<TKey>(this ICacheEntry<TKey> entry, Action<Task<CacheEvictionArgs<TKey>>, object?> action, object? state, CancellationToken cancellationToken = default)
+        public static ICacheEntry<TKey> ContinueWithOnEvicted<TKey>(this ICacheEntry<TKey> entry, Action<Task<CacheEvictionArgs<TKey>>, object?> action, object? state, CancellationToken cancellationToken = default) where TKey : notnull
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
@@ -84,7 +84,7 @@ namespace Outkeep.Core.Caching
         /// <summary>
         /// Convenience method to schedule a continuation on the <see cref="ICacheEntry.Evicted"/> property using the current task scheduler.
         /// </summary>
-        public static ICacheEntry<TKey> ContinueWithOnEvicted<TKey>(this ICacheEntry<TKey> entry, Action<Task<CacheEvictionArgs<TKey>>> action, CancellationToken cancellationToken = default)
+        public static ICacheEntry<TKey> ContinueWithOnEvicted<TKey>(this ICacheEntry<TKey> entry, Action<Task<CacheEvictionArgs<TKey>>> action, CancellationToken cancellationToken = default) where TKey : notnull
         {
             return ContinueWithOnEvicted(entry, action, out _, cancellationToken);
         }
@@ -92,7 +92,7 @@ namespace Outkeep.Core.Caching
         /// <summary>
         /// Convenience method to schedule a continuation on the <see cref="ICacheEntry.Evicted"/> property using the current task scheduler.
         /// </summary>
-        public static ICacheEntry<TKey> ContinueWithOnEvicted<TKey>(this ICacheEntry<TKey> entry, Action<Task<CacheEvictionArgs<TKey>>> action, out Task continuation, CancellationToken cancellationToken = default)
+        public static ICacheEntry<TKey> ContinueWithOnEvicted<TKey>(this ICacheEntry<TKey> entry, Action<Task<CacheEvictionArgs<TKey>>> action, out Task continuation, CancellationToken cancellationToken = default) where TKey : notnull
         {
             if (entry == null) throw new ArgumentNullException(nameof(entry));
 
