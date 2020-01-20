57abbb4f53a5674cd0b1cf276766e96f331dc6bc
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-20 19:25:22 +0000

    Added NullGrainStorageServiceCollectionExtensions

---
+++ src/Outkeep.Core/Storage/NullGrainStorageServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Orleans.Providers;
+using Orleans.Runtime;
+using Orleans.Storage;
+
+namespace Outkeep.Core.Storage
+{
+    public static class NullGrainStorageServiceCollectionExtensions
+    {
+        public static IServiceCollection AddNullGrainStorage(this IServiceCollection services, string name)
+        {
+            return services.AddSingletonNamedService<IGrainStorage, NullGrainStorage>(name);
+        }
+
+        public static IServiceCollection AddNullGrainStorageAsDefault(this IServiceCollection services)
+        {
+            return services.AddNullGrainStorage(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME);
+        }
+    }
+}
