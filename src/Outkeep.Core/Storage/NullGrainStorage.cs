b00b28e26633e9612f4a2f88898e6d1a9ca848bc
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-20 19:15:00 +0000

    Added NullGrainStorage

---
+++ src/Outkeep.Core/Storage/NullGrainStorage.cs
@@ -0,0 +1,32 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using Orleans.Storage;
+using System.Threading.Tasks;
+
+namespace Outkeep.Core.Storage
+{
+    /// <summary>
+    /// Implements an <see cref="IGrainStorage"/> service that does not write or read state.
+    /// For use with testing or to disable persistence where applicable.
+    /// </summary>
+    internal sealed class NullGrainStorage : IGrainStorage
+    {
+        /// <inheritdoc />
+        public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
