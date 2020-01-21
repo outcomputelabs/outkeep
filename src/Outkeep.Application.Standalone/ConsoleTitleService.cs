0b927732629b4cb46b49f61f76b007fe8ad6c6e5
Jorge Candeias <candeias@fastmail.fm>
2020-01-21 00:21:04 +0000

    Added ConsoleTitleService

---
+++ src/Outkeep.Application.Standalone/ConsoleTitleService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Outkeep.Api.Http;
+using Outkeep.Application.Standalone.Properties;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Outkeep.Application.Standalone
+{
+    /// <summary>
+    /// A simple startup task to show the active ports on the console title for developer convenience.
+    /// </summary>
+    internal class ConsoleTitleService : IHostedService
+    {
+        private readonly OutkeepHttpApiServerOptions _httpApiOptions;
+
+        public ConsoleTitleService(IOptions<OutkeepHttpApiServerOptions> httpApiOptions)
+        {
+            _httpApiOptions = httpApiOptions?.Value ?? throw new ArgumentNullException(nameof(httpApiOptions));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.Title = Resources.Console_Title.Format(_httpApiOptions.ApiUri?.Port ?? -1);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
