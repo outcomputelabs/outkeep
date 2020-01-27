645abb5420fb8bd8b149486add83240cba18bf2e
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-27 14:08:39 +0000

    Coverage

---
+++ test/Outkeep.Core.Tests/SafeTimerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outkeep.Core.Tests
+{
+    public class SafeTimerTests
+    {
+        [Fact]
+        public void ThrowsOnNullCallback()
+        {
+            // arrange
+            Func<object?, Task>? callback = null;
+
+            // act
+            Assert.Throws<ArgumentNullException>(nameof(callback), () =>
+            {
+                // act
+                new SafeTimer(callback!, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            });
+        }
+    }
+}
