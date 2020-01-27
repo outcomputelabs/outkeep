3b322aec1c2cdc378dad18efedaa8aafa983c464
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-27 18:57:57 +0000

    Added test

--- test/Outkeep.Core.Tests/SafeTimerTests.cs
+++ test/Outkeep.Core.Tests/SafeTimerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using System;
 using Xunit;
 
 namespace Outkeep.Core.Tests
@@ -8,11 +9,22 @@ namespace Outkeep.Core.Tests
         [Fact]
         public void ThrowsOnNullLogger()
         {
-            // act
+            // assert
             Assert.Throws<ArgumentNullException>("logger", () =>
             {
                 // act
                 new SafeTimer(null!, null!, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            });
+        }
+
+        [Fact]
+        public void ThrowsOnNullCallback()
+        {
+            // assert
+            Assert.Throws<ArgumentNullException>("callback", () =>
+            {
+                // act
+                new SafeTimer(NullLogger<SafeTimer>.Instance, null!, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
             });
         }
     }
