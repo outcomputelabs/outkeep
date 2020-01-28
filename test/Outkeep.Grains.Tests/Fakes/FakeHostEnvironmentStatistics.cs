7669d4b7a2a3e32fa5d6bc6cbffdd29cbc4117ca
Jorge Candeias <jorge.candeias@outcompute.com>
2020-01-28 14:17:09 +0000

    Coverage

---
+++ test/Outkeep.Grains.Tests/Fakes/FakeHostEnvironmentStatistics.cs
@@ -0,0 +1,13 @@
+﻿using Orleans.Statistics;
+
+namespace Outkeep.Grains.Tests.Fakes
+{
+    public class FakeHostEnvironmentStatistics : IHostEnvironmentStatistics
+    {
+        public long? TotalPhysicalMemory { get; set; }
+
+        public float? CpuUsage { get; set; }
+
+        public long? AvailableMemory { get; set; }
+    }
+}
---
+++ test/Outkeep.Grains.Tests/MemoryPressureMonitorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using Outkeep.Governance.Memory;
+using Outkeep.Grains.Tests.Fakes;
+using Xunit;
+
+namespace Outkeep.Grains.Tests
+{
+    public class MemoryPressureMonitorTests
+    {
+        [Fact]
+        public void IsUnderPressureReturnsFalseWhenAvailableMemoryIsUnknown()
+        {
+            // arrange
+            var stats = new FakeHostEnvironmentStatistics();
+            var options = new MemoryGovernanceOptions();
+            var monitor = new MemoryPressureMonitor(stats, Options.Create(options));
+
+            // act
+            var result = monitor.IsUnderPressure;
+
+            // assert
+            Assert.False(result);
+        }
+    }
+}
