782b0fb5b5b75418cf102b960c2136fc954d0715
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 15:55:53 +0000

    Coverage

---
+++ test/Outkeep.Grains.Interfaces.Tests/CachePulseTests.cs
@@ -0,0 +1,20 @@
+﻿using Outkeep.Caching;
+using System;
+using Xunit;
+
+namespace Outkeep.Grains.Interfaces.Tests
+{
+    public class CachePulseTests
+    {
+        [Fact]
+        public void Defaults()
+        {
+            // act
+            var pulse = new CachePulse();
+
+            // assert
+            Assert.Equal(Guid.Empty, pulse.Tag);
+            Assert.Null(pulse.Value.Value);
+        }
+    }
+}
