1ac2545d7c3b83f6da6b19110f37d31c6fd02b5b
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 14:16:13 +0000

    Coverage

--- test/Outkeep.Grains.Tests/ActivityStateTests.cs
+++ test/Outkeep.Grains.Tests/ActivityStateTests.cs
@@ -1,10 +1,28 @@
 ﻿using Outkeep.Governance;
+using System.Diagnostics.CodeAnalysis;
 using Xunit;
 
 namespace Outkeep.Grains.Tests
 {
     public class ActivityStateTests
     {
+        [Fact]
+        [SuppressMessage("Major Bug", "S1764:Identical expressions should not be used on both sides of a binary operator")]
+        public void EqualsSameObject()
+        {
+            // arrange
+            var state = new ActivityState
+            {
+                Priority = ActivityPriority.Normal
+            };
+
+            // act
+            var result = state.Equals(state);
+
+            // assert
+            Assert.True(result);
+        }
+
         [Fact]
         public void EqualsSameValue()
         {
