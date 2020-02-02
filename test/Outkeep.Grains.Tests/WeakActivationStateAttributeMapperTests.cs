d7028abfa48dc49ba05ab09a5d201447227b567e
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 20:35:09 +0000

    Coverage

---
+++ test/Outkeep.Grains.Tests/WeakActivationStateAttributeMapperTests.cs
@@ -0,0 +1,17 @@
+﻿using Outkeep.Governance;
+using System;
+using Xunit;
+
+namespace Outkeep.Grains.Tests
+{
+    public class WeakActivationStateAttributeMapperTests
+    {
+        [Fact]
+        public void GetFactoryThrowsOnNullParameter()
+        {
+            var mapper = new WeakActivationStateAttributeMapper();
+
+            Assert.Throws<ArgumentNullException>("parameter", () => mapper.GetFactory(null!, null!));
+        }
+    }
+}
