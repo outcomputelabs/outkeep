811771b4f4668a07710ec6ca123077471831a1c3
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 01:05:12 +0000

    Coverage

---
+++ test/Outkeep.Grains.Tests/WeakActivationStateFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Outkeep.Governance;
+using Outkeep.Grains.Tests.Fakes;
+using System;
+using Xunit;
+
+namespace Outkeep.Grains.Tests
+{
+    public class WeakActivationStateFactoryTests
+    {
+        [Fact]
+        public void ThrowsOnNullContext()
+        {
+            // arrange
+            var factory = new WeakActivationStateFactory();
+
+            // act
+            void action() => factory.Create<FakeWeakActivationFactor>(null!, null!);
+
+            // assert
+            Assert.Throws<ArgumentNullException>("context", action);
+        }
+    }
+}
