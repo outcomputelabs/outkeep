62101afd7f642dfd4a64e0bdc2621f1db3b0957c
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 17:41:09 +0000

    Coverage

--- test/Outkeep.Grains.Interfaces.Tests/CachePulseTests.cs
+++ test/Outkeep.Grains.Interfaces.Tests/CachePulseTests.cs
@@ -1,5 +1,6 @@
 ﻿using Outkeep.Caching;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Outkeep.Grains.Interfaces.Tests
@@ -15,6 +16,69 @@ namespace Outkeep.Grains.Interfaces.Tests
             // assert
             Assert.Equal(Guid.Empty, pulse.Tag);
             Assert.Null(pulse.Value);
+        }
+
+        [Fact]
+        public void Values()
+        {
+            // arrange
+            var tag = Guid.NewGuid();
+            var value = Guid.NewGuid().ToByteArray();
+
+            // act
+            var pulse = new CachePulse(tag, value);
+
+            // assert
+            Assert.Equal(tag, pulse.Tag);
+            Assert.Equal(value, pulse.Value);
+        }
+
+        [Fact]
+        public void OperatorsWork()
+        {
+            // arrange
+            CachePulse left;
+            CachePulse right;
+
+            // assert empty vs empty
+            left = new CachePulse();
+            right = new CachePulse();
+            Assert.True(Equals(left, right));
+            Assert.True(EqualityComparer<CachePulse>.Default.Equals(left, right));
+            Assert.True(left == right);
+            Assert.False(left != right);
+
+            // assert empty vs random
+            left = new CachePulse();
+            right = new CachePulse(Guid.NewGuid(), Guid.NewGuid().ToByteArray());
+            Assert.False(Equals(left, right));
+            Assert.False(EqualityComparer<CachePulse>.Default.Equals(left, right));
+            Assert.False(left == right);
+            Assert.True(left != right);
+
+            // assert random vs empty
+            left = new CachePulse(Guid.NewGuid(), Guid.NewGuid().ToByteArray());
+            right = new CachePulse();
+            Assert.False(Equals(left, right));
+            Assert.False(EqualityComparer<CachePulse>.Default.Equals(left, right));
+            Assert.False(left == right);
+            Assert.True(left != right);
+
+            // assert random vs random
+            left = new CachePulse(Guid.NewGuid(), Guid.NewGuid().ToByteArray());
+            right = new CachePulse(Guid.NewGuid(), Guid.NewGuid().ToByteArray());
+            Assert.False(Equals(left, right));
+            Assert.False(EqualityComparer<CachePulse>.Default.Equals(left, right));
+            Assert.False(left == right);
+            Assert.True(left != right);
+
+            // assert random vs same
+            left = new CachePulse(Guid.NewGuid(), Guid.NewGuid().ToByteArray());
+            right = left;
+            Assert.True(Equals(left, right));
+            Assert.True(EqualityComparer<CachePulse>.Default.Equals(left, right));
+            Assert.True(left == right);
+            Assert.False(left != right);
         }
     }
 }
