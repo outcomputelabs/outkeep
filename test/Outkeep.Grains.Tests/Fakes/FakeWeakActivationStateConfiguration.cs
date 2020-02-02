92072bc83e2bff894c957f2a2f62beece75415a1
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 12:25:55 +0000

    Coverage

---
+++ test/Outkeep.Grains.Tests/Fakes/FakeWeakActivationStateConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Outkeep.Governance;
+
+namespace Outkeep.Grains.Tests.Fakes
+{
+    public class FakeWeakActivationStateConfiguration : IWeakActivationStateConfiguration
+    {
+        public string? ResourceGovernorName { get; set; }
+    }
+}
--- test/Outkeep.Grains.Tests/WeakActivationStateFactoryTests.cs
+++ test/Outkeep.Grains.Tests/WeakActivationStateFactoryTests.cs
@@ -1,5 +1,7 @@
-﻿using Outkeep.Governance;
+﻿using Orleans.Runtime;
+using Outkeep.Governance;
 using Outkeep.Grains.Tests.Fakes;
+using Outkeep.Properties;
 using System;
 using Xunit;
 
@@ -18,6 +20,89 @@ namespace Outkeep.Grains.Tests
 
             // assert
             Assert.Throws<ArgumentNullException>("context", action);
+        }
+
+        [Fact]
+        public void ThrowsOnNullConfig()
+        {
+            // arrange
+            var factory = new WeakActivationStateFactory();
+            var context = new FakeGrainActivationContext();
+
+            // act
+            void action() => factory.Create<FakeWeakActivationFactor>(context, null!);
+
+            // assert
+            Assert.Throws<ArgumentNullException>("config", action);
+        }
+
+        [Fact]
+        public void ThrowsOnNonRegisteredDefaultResourceGovernor()
+        {
+            // arrange
+            var factory = new WeakActivationStateFactory();
+            var context = new FakeGrainActivationContext()
+            {
+                GrainType = typeof(object),
+                ActivationServices = new FakeServiceProvider()
+            };
+            var config = new FakeWeakActivationStateConfiguration();
+
+            // act
+            void action() => factory.Create<FakeWeakActivationFactor>(context, config);
+
+            // assert
+            var result = Assert.Throws<BadWeakActivationConfigException>(action);
+            Assert.Equal(Resources.Exception_NoDefaultResourceGovernorFoundForGrainType_X.Format(typeof(object).FullName!), result.Message);
+        }
+
+        [Fact]
+        public void ThrowsOnNonRegisteredNamedResourceGovernor()
+        {
+            // arrange
+            var factory = new WeakActivationStateFactory();
+            var context = new FakeGrainActivationContext()
+            {
+                GrainType = typeof(object),
+                ActivationServices = new FakeServiceProvider()
+            };
+            var config = new FakeWeakActivationStateConfiguration
+            {
+                ResourceGovernorName = "FakeProvider"
+            };
+
+            // act
+            void action() => factory.Create<FakeWeakActivationFactor>(context, config);
+
+            // assert
+            var result = Assert.Throws<BadWeakActivationConfigException>(action);
+            Assert.Equal(Resources.Exception_NoResourceGovernorNamed_X_FoundForGrainType_X.Format("FakeProvider", typeof(object).FullName!), result.Message);
+        }
+
+        [Fact]
+        public void CreatesDefaultState()
+        {
+            // arrange
+            var factory = new WeakActivationStateFactory();
+            var governor = new FakeResourceGovernor();
+            var lifecycle = new FakeGrainLifecycle();
+            var context = new FakeGrainActivationContext()
+            {
+                GrainType = typeof(object),
+                ActivationServices = new FakeServiceProvider((typeof(IResourceGovernor), () => governor)),
+                ObservableLifecycle = lifecycle
+            };
+            var config = new FakeWeakActivationStateConfiguration();
+
+            // act
+            var state = factory.Create<FakeWeakActivationFactor>(context, config);
+
+            // assert
+            Assert.NotNull(state);
+            var (observerName, stage, observer) = Assert.Single(lifecycle.Subscriptions);
+            Assert.Equal(typeof(WeakActivationState<FakeWeakActivationFactor>).FullName, observerName);
+            Assert.Equal(GrainLifecycleStage.SetupState, stage);
+            Assert.Same(state, observer);
         }
     }
 }
