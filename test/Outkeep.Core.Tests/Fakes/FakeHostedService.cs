6496d63defc1b079c74698f3249aca2bb37cc3e3
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 19:28:52 +0000

    Coverage

---
+++ test/Outkeep.Core.Tests/Fakes/FakeHostedService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Outkeep.Core.Tests.Fakes
+{
+    public class FakeHostedService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
