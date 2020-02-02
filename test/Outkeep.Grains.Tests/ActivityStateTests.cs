94b389d46a03bc004ba54ff1c7502290278c8903
Jorge Candeias <candeias@fastmail.fm>
2020-02-02 14:13:56 +0000

    Coverage

---
+++ test/Outkeep.Grains.Tests/ActivityStateTests.cs
@@ -0,0 +1,28 @@
+﻿using Outkeep.Governance;
+using Xunit;
+
+namespace Outkeep.Grains.Tests
+{
+    public class ActivityStateTests
+    {
+        [Fact]
+        public void EqualsSameValue()
+        {
+            // arrange
+            var left = new ActivityState
+            {
+                Priority = ActivityPriority.Normal
+            };
+            var right = new ActivityState
+            {
+                Priority = ActivityPriority.Normal
+            };
+
+            // act
+            var result = left.Equals(right);
+
+            // assert
+            Assert.True(result);
+        }
+    }
+}
