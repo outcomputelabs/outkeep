724cd7b8161dc55274a16a4df2445843707073e8
Jorge Candeias <jorge.candeias@outcompute.com>
2020-02-04 15:19:42 +0000

    Added extension

---
+++ samples/1.0/HelloWorldOrleans/Service/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans.Hosting;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class Program
+    {
+        private static Task Main()
+        {
+            return Host
+                .CreateDefaultBuilder()
+                .UseOrleans(orleans =>
+                {
+                    orleans.UseLocalhostClustering();
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
---
+++ src/Outkeep.Grains/OutkeepGrainsSiloBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Orleans;
+using Orleans.Hosting;
+using System;
+
+namespace Outkeep
+{
+    public static class OutkeepGrainsSiloBuilderExtensions
+    {
+        /// <summary>
+        /// Adds all Outkeep grains to this silo.
+        /// </summary>
+        public static ISiloBuilder AddOutkeepGrains(this ISiloBuilder builder)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            return builder.ConfigureApplicationParts(manager => manager.AddApplicationPart(typeof(OutkeepGrainsSiloBuilderExtensions).Assembly).WithReferences());
+        }
+    }
+}
