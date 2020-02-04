f8466d0a874b1661c73a364f11105401028fc2a6
Jorge Candeias <jorge.candeias@outcompute.com>
2020-02-04 23:20:50 +0000

    Added pulse string formatting

--- samples/1.0/HelloWorldOrleans/Service/Program.cs
+++ samples/1.0/HelloWorldOrleans/Service/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Concurrency;
 using Orleans.Hosting;
@@ -12,7 +13,7 @@ namespace Service
     {
         private static async Task Main()
         {
-            var host = Host
+            using var host = Host
                 .CreateDefaultBuilder()
                 .UseOrleans(orleans =>
                 {
@@ -27,9 +28,30 @@ namespace Service
 
             await host.StartAsync();
 
+            var logger = host.Services.GetService<ILogger<Program>>();
             var factory = host.Services.GetService<IGrainFactory>();
             var grain = factory.GetCacheGrain(Guid.NewGuid().ToString());
-            await grain.SetAsync(Guid.NewGuid().ToByteArray().AsImmutable(), null, null);
+
+            // cache a value
+            var value = Guid.NewGuid().ToByteArray();
+            logger.LogInformation("Setting Value {Value}", value);
+            await grain.SetAsync(value.AsImmutable(), null, null);
+
+            // read the cached value
+            var pulse = await grain.GetAsync();
+            logger.LogInformation("Received Pulse {Pulse}", pulse);
+
+            // wait for the next value
+            var task = grain.PollAsync(pulse.Tag);
+
+            // set the next value
+            value = Guid.NewGuid().ToByteArray();
+            logger.LogInformation("Setting Next Value {Value}", value);
+            await grain.SetAsync(value.AsImmutable(), null, null);
+
+            // verify the wait
+            pulse = await task;
+            logger.LogInformation("Received Reactive Pulse {Pulse}", pulse);
 
             await host.WaitForShutdownAsync();
         }
