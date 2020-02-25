e5ad2666e5257264e66ba56064a6f0635c06f31e
Jorge Candeias <candeias@fastmail.fm>
2020-02-25 00:45:51 +0000

    Adding standard registry

---
+++ src/Outkeep.Core.Abstractions/Registry/IRegistryGrainStorage.cs
@@ -0,0 +1,15 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using System.Threading.Tasks;
+
+namespace Outkeep.Registry
+{
+    public interface IRegistryGrainStorage
+    {
+        Task WriteEntryAsync(string grainType, GrainReference grainReference, IGrainState entry);
+
+        Task ReadEntryAsync(string grainType, GrainReference grainReference, IGrainState entry);
+
+        Task ClearEntryAsync(string grainType, GrainReference grainReference, IGrainState entry);
+    }
+}
---
+++ src/Outkeep.Grains/Registry/IRegistryGrainState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Outkeep.Registry
+{
+    public interface IRegistryGrainState<TEntry>
+    {
+        Task WriteAsync(string key, TEntry entry);
+
+        Task ClearAsync(string key);
+
+        Task<TEntry> ReadAsync(string key);
+
+        IAsyncEnumerable<TEntry> EnumerateAsync();
+    }
+}
